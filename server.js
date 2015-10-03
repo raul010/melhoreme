a1c21745aea41a817065d2c62efec4591654e881
Raul Leite <raul.abreu.leite@gmail.com>
2015-10-03 18:18:47 -0300

    disable node_modules cache

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
-var mongoose       = require('mongoose');
+//var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===========================================
 	
 // config files
-var db = require('./config/db');
+//var db = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
 // mongoose.connect(db.url); // connect to our mongoDB database (commented out after you enter in your own credentials)
