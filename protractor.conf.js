025b362cb3499d9a49ea2d878e7f8191fcfebc3d
Raul Leite <raul.abreu.leite@gmail.com>
2016-02-26 14:27:33 -0300

    Tests e2e and  specs

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,7 +2,7 @@ exports.config = {
     framework: 'mocha',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
-        './tests-e2e/**/*.e2e.js',
+        './tests-e2e/**/*.e2e.js'
     ],
     onPrepare: function() {
         browser.manage().window().setSize(1300, 700);
