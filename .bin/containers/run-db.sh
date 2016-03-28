7853edf68b09494d36f68736f7c675e1b3d630cd
Raul Leite <raul.abreu.leite@gmail.com>
2016-03-28 14:55:28 -0300

    up

--- .bin/containers/run-db.sh
+++ .bin/containers/run-db.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 # Stopping qualquer 27018 que estiver rodando
-mongo --port 27017 --eval 'db.adminCommand("shutdown")'
+nohup mongo --port 27017 --eval 'db.adminCommand("shutdown")' 2> /dev/null
 
 docker run -d -v "$(pwd)"/.bin/db-backup:/db-backup --name w-db --expose 27017 -p 27017:27017 -i -t raul010/w-db /bin/bash 
 docker exec -d -it w-db mongod 
-sleep 5
+
+# verifica se mongod está rodando, para poder prosseguir
+while [ `nc -z localhost 27017; echo $?` -eq 1 ]
+do
+    sleep 1
+    echo 'Waiting Connection...'
+done
+
 docker exec -it w-db  mongorestore --db admin /db-backup/admin; mongorestore --db melhoreme-test /db-backup/melhoreme-test
 
 docker exec -it w-db mongod --shutdown; 
